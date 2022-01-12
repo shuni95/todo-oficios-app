cfdf03b1faccdd2c4b4d3963ff4a7581264fa0b1
Junior Claudio Zavaleta <shuni95@protonmail.com>
2022-01-11 23:04:18 -0500

    Search results from API added

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -5,7 +5,7 @@ import { faBuilding } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function SearchResults() {
     let [searchParams, setSearchParams] = useSearchParams();
@@ -13,9 +13,21 @@ export function SearchResults() {
     const especialidades = useFetchEspecialidades();
     let [distrito, setDistrito] = useState(searchParams.get('distrito'));
     let [especialidad, setEspecialidad] = useState(searchParams.get('especialidad'));
+    let [resultados, setResultados] = useState([]);
+
+    useEffect(() => {
+        fetch(process.env.REACT_APP_BASE_URL + `/api/trabajadores?distrito=${distrito}&especialidad=${especialidad}`)
+        .then(response => response.json())
+        .then(results => {
+            console.info(results);
+            const newResultados = results.filter(r => r.distrito === parseInt(distrito) && r.idEspecialidad === parseInt(especialidad))
+
+            setResultados(newResultados);
+        });        
+    }, [distrito, especialidad])
     
     return (
-        <section className='flex flex-row h-full bg-gray-200 pt-8 xl:px-[150px]'>
+        <section className='flex flex-row h-full bg-gray-200 pt-8 xl:px-[150px] overflow-y-scroll'>
             <div className='flex flex-col sm:basis-2/5 lg:max-w-[320px] px-4 md:px-8'>
                 <form className='flex flex-col' onSubmit={() => setSearchParams({'distrito': distrito, 'especialidad': especialidad})}>
                 <h2 className='text-xl font-semibold'>Filtros</h2>
@@ -56,48 +68,23 @@ export function SearchResults() {
 
             <div className='flex flex-col sm:basis-3/5 lg:basis-full pb-20 space-y-4'>
                 <h2 className='text-xl font-bold pl-8'>Profesionales recomendados</h2>
-                
+                {resultados.map(trabajador => (
                     <div className='bg-white p-6 mx-8'>
                         <div>
-                            <h3 className='text-lg font-semibold text-blue-700'><Link to={`/professional/1`}>Antonio Vargas</Link></h3>
-                            <p className='mt-2'><FontAwesomeIcon icon={faMapMarkerAlt} color='red'/> <span className='ml-2'>El Agustino - </span><FontAwesomeIcon icon={faPhone} color='blue'/> <span className='ml-2'>989101989</span></p>
-                            <p><FontAwesomeIcon icon={faBuilding}/> <span className='ml-2'>Mz A Lt 21 Av. San Jeronimo</span></p>
-                            <p><FontAwesomeIcon icon={faEnvelope} color='silver'/> <span className='ml-2'>avargas@gmail.com</span></p>
-                            <p className='mt-2'>Ofrezco servicio 7u7 de gasfitería y electricidad, reparación e instalación de calefacción, destape de cámara de alcantarillado y cañerias de desague, trabajo en pvc y cobre. Interesados llamar al 989101989.</p>
+                            <h3 className='text-lg font-semibold text-blue-700'><Link to={`/professional/1`}>{trabajador.nombreTrabajador} {trabajador.apellidoTrabajador}</Link></h3>
+                            <p className='mt-2'>
+                                <FontAwesomeIcon icon={faMapMarkerAlt} color='red'/> <span className='ml-2'>{distritos.find(d => d.idDistrito === trabajador.distrito).nombreDistrito} - </span><FontAwesomeIcon icon={faPhone} color='blue'/><span className='ml-2'>{trabajador.telefono}</span>
+                            </p>
+                            <p><FontAwesomeIcon icon={faBuilding}/> <span className='ml-2'>{trabajador.direccion}</span></p>
+                            <p><FontAwesomeIcon icon={faEnvelope} color='silver'/> <span className='ml-2'>{trabajador.correo}</span></p>
+                            <p className='mt-2'>{trabajador.sobreMi}</p>
                         </div>
                         <div className='mt-2 flex flex-row'>
-                            <p className='text-gray-700 border border-solid border-black px-3 py-1'>Gasfitero</p>
+                            <p className='text-gray-700 border border-solid border-black px-3 py-1'>{trabajador.nombreEspecialidad}</p>
                             <Link className='uppercase bg-red-700 px-4 py-1 text-white ml-auto xl:ml-8' to={`/professional/1?especialidad=${searchParams.get('especialidad')}&distrito=${searchParams.get('distrito')}`}>Ver perfil</Link>
                         </div>
                     </div>
-                
-                    <div className='bg-white p-6 mx-8'>
-                        <div>
-                            <h3 className='text-lg font-semibold'>Antonio Vargas</h3>
-                            <p className='mt-2'><FontAwesomeIcon icon={faMapMarkerAlt} color='red'/> <span className='ml-2'>El Agustino - </span><FontAwesomeIcon icon={faPhone} color='blue'/> <span className='ml-2'>989101989</span></p>
-                            <p><FontAwesomeIcon icon={faBuilding}/> <span className='ml-2'>Mz A Lt 21 Av. San Jeronimo</span></p>
-                            <p><FontAwesomeIcon icon={faEnvelope} color='silver'/> <span className='ml-2'>avargas@gmail.com</span></p>
-                            <p className='mt-2'>Ofrezco servicio 7u7 de gasfitería y electricidad, reparación e instalación de calefacción, destape de cámara de alcantarillado y cañerias de desague, trabajo en pvc y cobre. Interesados llamar al 989101989.</p>
-                        </div>
-                        <div className='mt-2 flex flex-row'>
-                            <p className='text-gray-700 border border-solid border-black px-3 py-1'>Gasfitero</p>
-                            <button className='uppercase bg-red-700 px-4 py-1 text-white ml-auto xl:ml-8'>Ver perfil</button>
-                        </div>
-                    </div>
-
-                    <div className='bg-white p-6 mx-8'>
-                        <div>
-                            <h3 className='text-lg font-semibold'>Antonio Vargas</h3>
-                            <p className='mt-2'><FontAwesomeIcon icon={faMapMarkerAlt} color='red'/> <span className='ml-2'>El Agustino - </span><FontAwesomeIcon icon={faPhone} color='blue'/> <span className='ml-2'>989101989</span></p>
-                            <p><FontAwesomeIcon icon={faBuilding}/> <span className='ml-2'>Mz A Lt 21 Av. San Jeronimo</span></p>
-                            <p><FontAwesomeIcon icon={faEnvelope} color='silver'/> <span className='ml-2'>avargas@gmail.com</span></p>
-                            <p className='mt-2'>Ofrezco servicio 7u7 de gasfitería y electricidad, reparación e instalación de calefacción, destape de cámara de alcantarillado y cañerias de desague, trabajo en pvc y cobre. Interesados llamar al 989101989.</p>
-                        </div>
-                        <div className='mt-2 flex flex-row'>
-                            <p className='text-gray-700 border border-solid border-black px-3 py-1'>Gasfitero</p>
-                            <button className='uppercase bg-red-700 px-4 py-1 text-white ml-auto xl:ml-8'>Ver perfil</button>
-                        </div>
-                    </div>
+                ))}
             </div>
         </section>
     )
