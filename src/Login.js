c791bf2b2a42bc96713b0f1220fd1f1086e11de6
Junior Claudio Zavaleta <shuni95@protonmail.com>
2022-01-16 22:16:29 -0500

    Login page added

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,40 @@
-import { Component } from "react";
+import { useState } from "react";
 
-export class Login extends Component {
-    render() {
-        return (
-            <div>
-                Login Page
-            </div>
-        )
+export function Login () {
+    const usuario = useFormInput("");
+    const password = useFormInput("");
+
+    function useFormInput(initValue) {
+        const [value, setValue] = useState(initValue);
+
+        function handleChange(e) {
+            setValue(e.target.value);
+        }
+
+        return {
+            value,
+            onChange: handleChange,
+        }
     }
+
+    return (
+        <section className="flex flex-col w-full items-center h-full bg-amber-200 mx-auto pt-4 md:pt-32">
+            <form className="flex flex-row justify-center w-full md:w-[540px] bg-white mx-12 rounded-2xl">
+                <div className="flex flex-col rounded-sm w-[80%] mt-4 pt-8 px-5 pb-20">
+                    <div className="flex flex-row justify-center">
+                        <h1 className="text-4xl font-bold">Login</h1>
+                    </div>
+                    <div className="flex flex-col mt-16">
+                        <input type="text" className="border-t-0 border-x-0 border-b-2" name="usuario" placeholder="Usuario" {...usuario}/>
+                    </div>
+                    <div className="flex flex-col mt-8">
+                        <input type="password" className="border-t-0 border-x-0 border-b-2" name="password" placeholder="Password" {...password}/>
+                    </div>
+                    <div className="flex flex-row mt-10 justify-center">
+                        <button className='w-[200px] text-lg p-2 bg-emerald-400 text-white uppercase rounded-full select-none'>Ingresar</button>
+                    </div>
+                </div>
+            </form>
+        </section>
+    )
 }
